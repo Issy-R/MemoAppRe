7cbae223ae5de6aacf672c9b5d830120ab21d014
issy <77483468+Issy-R@users.noreply.github.com>
2021-03-06 22:14:38 +0900

    Create 2Screen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListlScreen from './src/screens/MemoListScreen';
-
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListlScreen />
+    <MemoCreateScreen />
   );
 }
---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  StyleSheet, TextInput, View, KeyboardAvoidingView,
+} from 'react-native';
+import AppBar from '../components/AppBar';
+import CircleBotton from '../components/CircleBotton';
+
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input} />
+      </View>
+      <CircleBotton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
