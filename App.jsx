99755fda1ab445b8f9c377e796a3c481d72625d6
issy <77483468+Issy-R@users.noreply.github.com>
2021-03-08 19:36:02 +0900

    Create Login & SignUp

--- App.jsx
+++ App.jsx
@@ -3,9 +3,11 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListlScreen from './src/screens/MemoListScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
